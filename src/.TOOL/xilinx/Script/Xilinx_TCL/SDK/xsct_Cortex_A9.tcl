0f92a238cf455f8ef6b797904f50fd804e50325c
sterben <1579729633@qq.com>
2020-04-18 21:13:52 +0800

    2020/4/18

--- src/.TOOL/xilinx/Script/Xilinx_TCL/SDK/xsct_Cortex_A9.tcl
+++ src/.TOOL/xilinx/Script/Xilinx_TCL/SDK/xsct_Cortex_A9.tcl
@@ -1,29 +1,28 @@
+set hw_name  zynq_wrapper
+set bsp_name BSP_package
+set prj_name test
 # Set SDK workspace
 setws ./user/Software/src
 # Create a HW project
-createhw -name hw1 -hwspec /tmp/system.hdf
+createhw -name $hw_name -hwspec ./user/Software/data/zynq_wrapper.hdf
 # Create a BSP project
-createbsp -name bsp1 -hwproject hw1 -proc ps7_cortexa9_0 -os standalone
+createbsp -name $bsp_name -hwproject $hw_name -proc ps7_cortexa9_0 -os standalone
 # Create application project
-createapp -name hello -hwproject hw1 -bsp bsp1 -proc ps7_cortexa9_0 -os standalone \
+createapp -name $prj_name -hwproject $hw_name -bsp $bsp_name -proc ps7_cortexa9_0 -os standalone \
 -lang C -app {Hello World}
 # Build all projects
 projects -build
 # Connect to a remote hw_server
-connect -host raptor-host
+connect
 # Select a target
-targets -set -nocase -filter {name =~ “ARM* #0}
-# System Reset
+targets 2
+#System Reset
 rst -system
 # PS7 initialization
-namespace eval xsdb {source /tmp/workspace/hw1/ps7_init.tcl; ps7_init}
+namespace eval xsdb \
+{ 
+	source ./user/Software/src/$hw_name/ps7_init.tcl
+	ps7_init
+}
 # Download the elf
-dow /tmp/workspace/hello/Debug/hello.elf
-# Insert a breakpoint @ main
-bpadd -addr &main
-# Continue execution until the target is suspended
-con -block -timeout 500
-# Print the target registers
-puts [rrd]
-# Resume the target
-con
+#dow ./user/Software/src/$prj_name/Debug/$prj_name.elf
