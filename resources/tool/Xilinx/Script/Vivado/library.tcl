a8c2aa43edfddefb01374fcc894e965a157973a3
sterben <1579729633@qq.com>
2021-05-25 19:03:45 +0800

    reconstruction init

--- resources/tool/Xilinx/Script/Vivado/library.tcl
+++ resources/tool/Xilinx/Script/Vivado/library.tcl
@@ -1,1 +1,1 @@
-add_file d:/project/Code/.prj/Plug/My_Plug/FPGA_Support/out/../lib/src_lib/Hardware/Math/Div.v
+add_file d:/project/Code/.prj/Plug/My_Plug/FPGA_Support/src/../lib/src_lib/Hardware/Math/Div.v
