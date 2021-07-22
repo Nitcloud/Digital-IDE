667225b08f08aaa34061335eb00aef89b137f666
sterben <1579729633@qq.com>
2021-07-23 00:10:39 +0800

    fix some parts

--- src/extension.js
+++ src/extension.js
@@ -21,15 +21,15 @@ function activate(context) {
     let HDLFileList = [];
     let opeParam = {
         "os"             : "",
-        "prjInfo"        : null,
         "rootPath"       : "",
         "workspacePath"  : "",
-        "currentSrcPath" : "",
+        "prjInfo"        : null,
+        "currentHDLPath" : [],
         "prjInitParam"   : "",
         "propertyPath"   : ""
     }
     filesys.prjs.getOpeParam(`${__dirname}`,opeParam);
-    filesys.prjs.refreshPrjFiles(opeParam, HDLFileList);
+    filesys.prjs.getPrjFiles(opeParam, HDLFileList);
 
 	// Output Channel
 	var outputChannel = vscode.window.createOutputChannel("HDL");
@@ -44,34 +44,29 @@ function activate(context) {
     tool.registerToolServer(opeParam);
     tool.registerSocServer(opeParam);
 
-    let parse = new parser.ParserLib.ParserFactory;
-
     try {
-        parse.getParser("verilog").then((lang_parser)=>{
-            console.time('timer');
-            const indexer = new parser.indexer(statusBar, HDLparam, lang_parser);
-            indexer.build_index(HDLFileList).then(() => {
-                console.timeEnd('timer');
-                // console.log(indexer.HDLparam);
-                // console.log(indexer.symbols);
-                var vlogComplete = new tool.lspCompletion.vlogCompletion(indexer.HDLparam);
-                var fileExplorer = new tool.tree.FileExplorer(indexer.HDLparam, opeParam);
-                let watcher = filesys.monitor.monitor(opeParam.workspacePath, opeParam, indexer, outputChannel, () => {
-                    vlogComplete.HDLparam = indexer.HDLparam;
-                    fileExplorer.treeDataProvider.HDLparam = indexer.HDLparam;
-                    fileExplorer.treeDataProvider.refresh();
-                });
-            //    let processLib = new filesys.prjs.processLib(opeParam, watcher);
-            //     filesys.monitor.processPropertyFile(opeParam, indexer, processLib); 
-                // linter Server
-                // new linter.registerLinterServer("vhdl", "linter", context);
-                // project Server
-                filesys.registerPrjsServer(context, opeParam);
-                // tool Server
-                tool.registerSimServer(indexer, opeParam);
-                tool.registerLspServer(context, indexer, vlogComplete);
-                tool.registerBuildServer(context, indexer, opeParam);
+        console.time('timer');
+        const indexer = new parser.indexer(statusBar, HDLparam);
+        indexer.build_index(HDLFileList).then(() => {
+            // indexer.refreshInstModulePath(indexer.HDLparam);
+            console.timeEnd('timer');
+            console.log(indexer.HDLparam);
+            console.log(indexer.symbols);
+            var vlogComplete = new tool.lspCompletion.vlogCompletion(indexer.HDLparam);
+            var fileExplorer = new tool.tree.FileExplorer(indexer.HDLparam, opeParam);
+            filesys.monitor.monitor(opeParam.workspacePath, opeParam, indexer, outputChannel, () => {
+                vlogComplete.HDLparam = indexer.HDLparam;
+                fileExplorer.treeDataProvider.HDLparam = indexer.HDLparam;
+                fileExplorer.treeDataProvider.refresh();
             });
+            // linter Server
+            // new linter.registerLinterServer("vhdl", "linter", context);
+            // project Server
+            filesys.registerPrjsServer(context, opeParam);
+            // tool Server
+            tool.registerSimServer(indexer, opeParam);
+            tool.registerLspServer(context, indexer, vlogComplete);
+            tool.registerBuildServer(context, indexer, opeParam);
         });
     } catch (error) {
         console.log(error);
