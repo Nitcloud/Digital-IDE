bece15bdd7d421920e46c3334931d33643e36e89
sterben <1579729633@qq.com>
2021-07-24 00:12:44 +0800

    fix DefinitionProvider

--- src/extension.js
+++ src/extension.js
@@ -28,49 +28,49 @@ function activate(context) {
         "prjInitParam"   : "",
         "propertyPath"   : ""
     }
-    filesys.prjs.getOpeParam(`${__dirname}`,opeParam);
-    filesys.prjs.getPrjFiles(opeParam, HDLFileList);
-
-	// Output Channel
-	var outputChannel = vscode.window.createOutputChannel("HDL");
-
-    // Status Bar
-    const statusBar = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 0);
-	context.subscriptions.push(statusBar);
+    if(filesys.prjs.getOpeParam(`${__dirname}`,opeParam) != null) {
+        filesys.prjs.getPrjFiles(opeParam, HDLFileList);
     
-    tool.registerXilinxServer(opeParam);
-    tool.registerDebugServer(opeParam);
-    tool.registerTreeServer(opeParam);
-    tool.registerToolServer(opeParam);
-    tool.registerSocServer(opeParam);
-
-    try {
-        console.time('timer');
-        const indexer = new parser.indexer(statusBar, HDLparam);
-        indexer.build_index(HDLFileList).then(() => {
-            // indexer.refreshInstModulePath(indexer.HDLparam);
-            console.timeEnd('timer');
-            console.log(indexer.HDLparam);
-            console.log(indexer.symbols);
-            var vlogComplete = new tool.lspCompletion.vlogCompletion(indexer.HDLparam);
-            var fileExplorer = new tool.tree.FileExplorer(indexer.HDLparam, opeParam);
-            filesys.monitor.monitor(opeParam.workspacePath, opeParam, indexer, outputChannel, () => {
-                vlogComplete.HDLparam = indexer.HDLparam;
-                fileExplorer.treeDataProvider.HDLparam = indexer.HDLparam;
-                fileExplorer.treeDataProvider.refresh();
+        // Output Channel
+        var outputChannel = vscode.window.createOutputChannel("HDL");
+    
+        // Status Bar
+        const statusBar = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 0);
+        context.subscriptions.push(statusBar);
+        
+        tool.registerXilinxServer(opeParam);
+        tool.registerDebugServer(opeParam);
+        tool.registerTreeServer(opeParam);
+        tool.registerToolServer(opeParam);
+        tool.registerSocServer(opeParam);
+    
+        try {
+            console.time('timer');
+            const indexer = new parser.indexer(statusBar, HDLparam);
+            indexer.build_index(HDLFileList).then(() => {
+                console.timeEnd('timer');
+                console.log(indexer.HDLparam);
+                console.log(indexer.symbols);
+                var vlogComplete = new tool.lspCompletion.vlogCompletion(indexer.HDLparam);
+                var fileExplorer = new tool.tree.FileExplorer(indexer.HDLparam, opeParam);
+                filesys.monitor.monitor(opeParam.workspacePath, opeParam, indexer, outputChannel, () => {
+                    vlogComplete.HDLparam = indexer.HDLparam;
+                    fileExplorer.treeDataProvider.HDLparam = indexer.HDLparam;
+                    fileExplorer.treeDataProvider.refresh();
+                });
+                // linter Server
+                // new linter.registerLinterServer("vhdl", "linter", context);
+                // project Server
+                filesys.registerPrjsServer(context, opeParam);
+                // tool Server
+                tool.registerSimServer(indexer, opeParam);
+                tool.registerLspServer(context, indexer, vlogComplete);
+                tool.registerBuildServer(context, indexer, opeParam);
             });
-            // linter Server
-            // new linter.registerLinterServer("vhdl", "linter", context);
-            // project Server
-            filesys.registerPrjsServer(context, opeParam);
-            // tool Server
-            tool.registerSimServer(indexer, opeParam);
-            tool.registerLspServer(context, indexer, vlogComplete);
-            tool.registerBuildServer(context, indexer, opeParam);
-        });
-    } catch (error) {
-        console.log(error);
-        return null;
+        } catch (error) {
+            console.log(error);
+            return null;
+        }
     }
 }
 exports.activate = activate;
