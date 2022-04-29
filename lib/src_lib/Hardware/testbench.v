3f4359054581a81379c7b38a3c8961ab1f402df0
sterben <1579729633@qq.com>
2022-04-29 15:47:38 +0800

    fix lsp

--- lib/src_lib/Hardware/testbench.v
+++ lib/src_lib/Hardware/testbench.v
@@ -3,45 +3,39 @@ module testbench();
 parameter DATA_WIDTH = 32;
 parameter ADDR_WIDTH = 32;
 parameter MAIN_FRE   = 100; //unit MHz
-reg                   clk       = 0;
-reg                   sys_rst_n = 0;
+reg                   sys_clk = 0;
+reg                   sys_rst = 1;
 reg [DATA_WIDTH-1:0]  data = 0;
 reg [ADDR_WIDTH-1:0]  addr = 0;
 
 always begin
-    #(500/MAIN_FRE) clk = ~clk;
-end
-always begin
-    #50 sys_rst_n = 1;
-end
-always begin
-    if (sys_rst_n) begin
-        #10 addr = addr + 1;#10;
-    end
-    else begin     
-        #10 addr = 0;#10;
-    end
-end
-always begin
-    if (sys_rst_n) begin
-        #10 data = data + 1;#10;
-    end
-    else begin     
-        #10 data = 0;#10;
-    end
+    #(500/MAIN_FRE) sys_clk = ~sys_clk;
 end
 
+always begin
+    #50 sys_rst = 0;
+end
 
-
+always @(posedge sys_clk) begin
+    if (sys_rst) 
+        addr = 0;
+    else      
+        addr = addr + 1;
+end
+always @(posedge sys_clk) begin
+    if (sys_rst) 
+        data = 0;
+    else      
+        data = data + 1;
+end
 
 //Instance 
-
-
 
 
 initial begin            
     $dumpfile("wave.vcd");        
     $dumpvars(0, testbench);    
+    #50000 $finish
 end
 
 endmodule  //TOP
--- lib/src_lib/Hardware/testbench.vhd
+++ lib/src_lib/Hardware/testbench.vhd
@@ -2,22 +2,15 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
--- Generated from Verilog module testbench (testbench.v:1)
---   ADDR_WIDTH = 32
---   DATA_WIDTH = 32
---   MAIN_FRE = 100
+
 entity testbench is
 end entity; 
 
--- Generated from Verilog module testbench (testbench.v:1)
---   ADDR_WIDTH = 32
---   DATA_WIDTH = 32
---   MAIN_FRE = 100
 architecture from_verilog of testbench is
-  signal addr : unsigned(31 downto 0) := X"00000000";  -- Declared at testbench.v:9
-  signal clk : std_logic := '0';  -- Declared at testbench.v:6
-  signal data : unsigned(31 downto 0) := X"00000000";  -- Declared at testbench.v:8
-  signal sys_rst_n : std_logic := '0';  -- Declared at testbench.v:7
+  signal sys_clk : std_logic := '0';  
+  signal sys_rst : std_logic := '0';  
+  signal addr : unsigned(31 downto 0) := X"00000000";  
+  signal data : unsigned(31 downto 0) := X"00000000";  
 begin
   -- Removed one empty process
   
@@ -26,20 +19,20 @@ begin
   process is
   begin
     wait for 5 ms;
-    clk <= not clk;
+    sys_clk <= not sys_clk;
   end process;
   
   -- Generated from always process in testbench (testbench.v:14)
   process is
   begin
     wait for 50 ms;
-    sys_rst_n <= '1';
+    sys_rst <= '1';
   end process;
   
   -- Generated from always process in testbench (testbench.v:17)
   process is
   begin
-    if sys_rst_n = '1' then
+    if sys_rst = '1' then
       wait for 10 ms;
       addr <= addr + X"00000001";
       wait for 10 ms;
@@ -53,7 +46,7 @@ begin
   -- Generated from always process in testbench (testbench.v:25)
   process is
   begin
-    if sys_rst_n = '1' then
+    if sys_rst = '1' then
       wait for 10 ms;
       data <= data + X"00000001";
       wait for 10 ms;
