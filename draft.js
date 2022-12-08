26a2b38d31cb66b5145e866e744866f81685c93a
Bestduan <1579729633@qq.com>
2022-12-08 14:39:36 +0800

    fix HDLparsr kernel

--- draft.js
+++ draft.js
@@ -1,12 +1,14 @@
-const util = require('./src/HDLparser/util');
+const parser = require("./src/HDLparser/util/kernel");
 const path = require('path');
 const fs = require('fs');
 
 TEST_VLOG_FILE = path.join(__dirname, 'test/vlog/test.v');
 
-const vlog = new util.VlogParser();
-let code = fs.readFileSync(TEST_VLOG_FILE, 'utf-8');
+const vlog = new parser.vlog();
+const vhdl = new parser.vhdl();
 
+let code = fs.readFileSync(TEST_VLOG_FILE, 'utf-8');
+console.log(code);
 let parse_result = vlog.parse(code);
 let symbol_result = vlog.symbol(code);
 
--- src/HDLparser/test.js
+++ src/HDLparser/test.js
@@ -5,12 +5,11 @@ const fs = require("fs");
 const vlog = new parser.vlog();
 const vhdl = new parser.vhdl();
 
-const path = "D:/Project/Code/Javascript/HDLparser/test/vhdl/test.vhd";
-// const path = "D:/Project/Code/Javascript/HDLparser/test/vlog/parse_test/Cordic.v";
+// const path = "D:/Project/Code/Javascript/HDLparser/test/vhdl/test.vhd";
+const path = "D:/Project/Code/.prj/Extension/Owner/Digital-IDE/test/vlog/test.v";
 // const path = "D:/Project/Code/Javascript/HDLparser/test/vlog/dependence_test/parent.v";
 const code = fs.readFileSync(path, "utf-8");
-console.log(code[-1]);
-const fast = vhdl.parse(code);
-const all  = vhdl.symbol(code);
+const fast = vlog.parse(code);
+const all  = vlog.symbol(code);
 console.log(fast);
 console.log(all);
